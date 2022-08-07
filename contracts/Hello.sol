9a610fb7f48dc92f505466ce8fb4b83dc5987b2a
Walter Pinson <walter@walterpinson.com>
2022-08-07 18:40:37 -0400

    Flesh out the Hello contact and maintain 100% unit test code coverage

--- contracts/Hello.sol
+++ contracts/Hello.sol
@@ -3,19 +3,22 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Hello {
+contract Hello is Ownable {
     using Counters for Counters.Counter;
     using Strings for uint256;
-    address public owner;
 
     Counters.Counter private _identityCounter;
 
     string public greeter;
 
     constructor() {
-        owner = msg.sender;
         greeter = "Jane Doe";
+    }
+
+    function greeterCount() external view returns(uint256) {
+        return _identityCounter.current();
     }
 
     function greeting() external view returns(string memory) {
@@ -31,5 +34,18 @@ contract Hello {
         );
 
         return output;
+    }
+
+    function changeGreeter(string memory newGreeter) public {
+        bytes memory b = bytes(newGreeter);
+        require(b.length > 0, "Invalid newGreeter. Empty string not allowed.");
+
+        greeter = newGreeter;
+        _identityCounter.increment();
+    }
+
+    function reset() external onlyOwner {
+        _identityCounter.reset();
+        greeter = "Jane Doe";
     }
 }
--- test/Hello.ts
+++ test/Hello.ts
@@ -13,19 +13,121 @@ describe("Hello", function () {
         return { hello, owner, otherAccount };
       }
 
+      const phrase1 = "Hello, World! My name is ";
+      const phrase2 = ". The greeter has changed ";
+      const phrase3 = " times.";
+      const defaultGreeter = "Jane Doe";
+      const defaultGreeterCount = 0;
+
     describe("Deployment", function () {
-        it("Should set the right owner", async function () {
-          const { hello, owner } = await loadFixture(deployHelloFixture);
-    
-          expect(await hello.owner()).to.equal(owner.address);
+        it(`Should set the right owner`, async () => {
+            // ARRANGE
+            // ACT
+            const { hello, owner } = await loadFixture(deployHelloFixture);
+
+            // ASSERT
+            expect(await hello.owner()).to.equal(owner.address);
+        });
+
+        it(`Should set the default greeter | ${defaultGreeter} `, async () => {
+            // ARRANGE
+            // ACT
+            const { hello } = await loadFixture(deployHelloFixture);
+
+            // ASSERT
+            expect(await hello.greeter()).to.equal(defaultGreeter);
+        });
+
+        it(`Should set the default greeterCount | ${defaultGreeterCount})`, async () => {
+            // ARRANGE
+            // ACT
+            const { hello } = await loadFixture(deployHelloFixture);
+
+            // ASSERT
+            expect(await hello.greeterCount()).to.equal(defaultGreeterCount);
         });
     });
 
     describe("Greeting", function () {
-        it("Should respond with the default greeting", async function () {
-            const { hello, owner } = await loadFixture(deployHelloFixture);
-            const defaultGreeting =  "Hello, World! My name is Jane Doe. The greeter has changed 0 times."
-            expect(await hello.greeting()).to.equal(defaultGreeting);
+        it("Should respond with the default greeting on first call", async () => {
+            // ARRANGE
+            const defaultGreeting =  `${phrase1}${defaultGreeter}${phrase2}${defaultGreeterCount}${phrase3}`
+            const { hello } = await loadFixture(deployHelloFixture);
+
+            // ACT
+            let greeting = await hello.greeting()
+
+            // ASSERT
+            expect(greeting).to.equal(defaultGreeting);
+        });
+        
+        describe("Change Geeter", function () {
+            const newGreeter = "John Doe";
+            const newGreeterCount = 3;
+
+            it(`Should set greeter | ${newGreeter}`, async () => {
+                // ARRANGE
+                const { hello } = await loadFixture(deployHelloFixture);
+
+                // ACT
+                await hello.changeGreeter(newGreeter);
+
+                // ASSERT
+                expect(await hello.greeter()).to.equal(newGreeter);
+            });
+
+            it(`Should set greeterCount | ${newGreeterCount}`, async () => {
+                // ARRANGE
+                const { hello } = await loadFixture(deployHelloFixture);
+
+                // ACT
+                await hello.changeGreeter("Some Greeter");
+                await hello.changeGreeter("Some Other Greeter");
+                await hello.changeGreeter(newGreeter);
+
+
+                // ASSERT
+                expect(await hello.greeterCount()).to.equal(newGreeterCount);
+            });
+
+            it(`Should revert with correct error when new greeter is empty`, async () => {
+                // ARRANGE
+                const revertMsg = "Invalid newGreeter. Empty string not allowed.";
+                const { hello } = await loadFixture(deployHelloFixture);
+
+                // ACT
+                // ASSERT
+                await expect(hello.changeGreeter("")).to.be.revertedWith(revertMsg);
+            });
+        });
+
+        describe("Reset", function () {
+            it(`Should reset the state back to the defaults`, async () => {
+                // ARRANGE
+                const { hello } = await loadFixture(deployHelloFixture);
+                await hello.changeGreeter("Greeter 1");
+                await hello.changeGreeter("Greeter 2");
+                await hello.changeGreeter("Greeter 3");
+                expect(await hello.greeter()).to.equal("Greeter 3");
+                expect(await hello.greeterCount()).to.equal(3);
+
+                // ACT
+                await hello.reset();
+
+                // ASSERT
+                expect(await hello.greeter()).to.equal(defaultGreeter);
+                expect(await hello.greeterCount()).to.equal(defaultGreeterCount);
+            });
+
+            it(`Should revert when non-owner makes call`, async () => {
+                // ARRANGE
+                const revertMsg = "Ownable: caller is not the owner";
+                const { hello, otherAccount } = await loadFixture(deployHelloFixture);
+
+                // ACT
+                // ASSERT
+                await expect(hello.connect(otherAccount).reset()).to.be.revertedWith(revertMsg);
+            });
         });
     });
 });
