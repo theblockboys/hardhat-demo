88f15c5c23d0dc743f2ec65de03debae6589bd04
Walter Pinson <walter@walterpinson.com>
2022-08-08 10:32:51 -0400

    Configure Goerli as a target deployment network. Use secrets file references to keep secrets out of GitHub.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import secrets from "./secrets.json";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: secrets.networks.goerli.endpoint,
+      accounts: [secrets.networks.goerli.accountPrivateKey]
+    }
+  }
 };
 
 export default config;
